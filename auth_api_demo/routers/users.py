008e7161a8e15eea6da782bac0256c358d25e9f7
Nguyễn Bảo Thạch <nguyenbaothach@MacBook-Pro-cua.local>
2026-06-19 15:03:57 +0700

    Update routers login & /token/refresh

--- auth_api_demo/routers/users.py
+++ auth_api_demo/routers/users.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, HTTPException
-from schemas.user_schema import UserRegister
+from schemas.user_schema import UserRegister, UserLogin
+from schemas.token_schema import TokenRefreshRequest
 from database.connection import get_connection
-from utils.password_hash import hash_password
+from utils.password_hash import hash_password, verify_password
+from utils.jwt_handler import create_access_token, create_refresh_token, verify_token
 
 router = APIRouter()
 
@@ -17,10 +19,53 @@ def register(user: UserRegister):
             hashed = hash_password(user.password)
 
             cursor.execute(
-                "INSERT INTO users (email, name, password_hash) VALUES (%s, %s, %s)",
-                (user.email, user.name, hashed)
+                "INSERT INTO users (email, name, password_hash, role) VALUES (%s, %s, %s, %s)",
+                (user.email, user.name, hashed, user.role)
             )
         conn.commit()  # Lưu thay đổi vào DB
     finally:
         conn.close()   # Luôn đóng kết nối
     return {"message": "Đăng ký thành công"}
+
+@router.post("/login")
+def login(user: UserLogin):
+    conn = get_connection()
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT * FROM users WHERE email = %s", (user.email,))
+            db_user = cursor.fetchone()
+            
+            if not db_user or not verify_password(user.password, db_user["password_hash"]):
+                raise HTTPException(status_code=401, detail="Email hoặc mật khẩu không chính xác")
+            
+            # 3. Sinh token (truyền thông tin cần thiết vào payload)
+            payload = {"sub": db_user["email"], "role": db_user["role"]}
+            access_token = create_access_token(data=payload)
+            refresh_token = create_refresh_token(data=payload)
+            
+            # 4. Trả về token
+            return {
+                "access_token": access_token,
+                "refresh_token": refresh_token,
+                "token_type": "bearer"
+            }
+    finally:
+        conn.close()
+
+@router.post("/token/refresh")
+def refresh_token(request: TokenRefreshRequest):
+    payload = verify_token(request.refresh_token, expected_type="refresh")
+    
+    if not payload:
+        raise HTTPException(status_code=401, detail="Refresh token không hợp lệ hoặc đã hết hạn")
+    
+    user_email = payload.get("sub")
+    role = payload.get("role")
+    
+    new_payload = {"sub": user_email, "role": role}
+    new_access_token = create_access_token(data=new_payload)
+    
+    return {
+        "access_token": new_access_token,
+        "token_type": "bearer"
+    }
--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,13 +1,25 @@
-from pydantic import BaseModel, EmailStr, model_validator
+from pydantic import BaseModel, EmailStr, Field, model_validator, field_validator
 
 class UserRegister(BaseModel):
     email: EmailStr
-    name: str
-    password: str
-    confirm_password: str
+    name: str 
+    password: str = Field(min_length = 8)
+    role: str = "user"
+    confirm_password: str = Field(min_length = 8)
+
+    @field_validator("name")
+    @classmethod
+    def name_must_not_contain_numbers(cls, v):
+        if any(char.isdigit() for char in v):
+            raise ValueError("Tên không được chứa chữ số")
+        return v
 
     @model_validator(mode="after")
     def check_password_match(self):
         if self.password != self.confirm_password:
             raise ValueError("Password và Confirm Password không khớp")
         return self
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
